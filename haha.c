ab0600d6e42b58cddc3218e6987eb94d63514b31
liweiyin-2019 <584510445@qq.com>
2019-10-25 11:40:52 +0800

    haha

---
+++ haha.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	printf("this is a method to solve the request to input the username and password:\n");
+	printf("git remote -v\n");
+	printf("git remote rm origin\n");
+	printf("git remote add origin git@github.com:liweiyin-2019/testgithub20191025.git \n");
+	return 0;
+}
